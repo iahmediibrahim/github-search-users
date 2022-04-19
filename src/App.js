f5b2b885116c49084f1711d3fa21b5862e480867
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-19 02:36:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Dashboard, Error, Login } from './pages'
+// PrivateRoute,
+// 	AuthWrapper,
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route path='/' exact>
+					<Dashboard />
+				</Route>
+				<Route path='login'>
+					<Login />
+				</Route>
+				<Route path='*'>
+					<Error />
+				</Route>
+			</Switch>
+		</Router>
+	)
+}
+
+export default App
