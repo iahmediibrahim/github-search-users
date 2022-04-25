c66783956d4ea49a40cf4ec811beef328e63667a
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-25 12:15:01 +0200

    checking for request rate

--- src/context/context.js
+++ src/context/context.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from 'react'
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
 import mockFollowers from './mockData.js/mockFollowers'
 import mockRepos from './mockData.js/mockRepos'
 import mockUser from './mockData.js/mockUser'
-// import axios from 'axios';
 
 const rootUrl = 'https://api.github.com'
 
@@ -13,12 +13,37 @@ const GithubProvider = ({ children }) => {
 	const [githubUser, setGithubUser] = useState(mockUser)
 	const [repos, setRepos] = useState(mockRepos)
 	const [followers, setFollowers] = useState(mockFollowers)
+	// request loading
+	const [requests, setRequests] = useState(0)
+	const [loading, setLoading] = useState(false)
+	// check rate
+	const checkRequests = () => {
+		axios(`${rootUrl}/rate_limit`)
+			.then(
+				({
+					data: {
+						rate: { remaining },
+					},
+				}) => {
+					console.log(remaining)
+					setRequests(remaining)
+					if (remaining === 0) {
+						// throw an error
+					}
+				},
+			)
+			.catch((err) => console.log(err))
+	}
+	// error
+	useEffect(checkRequests, [])
+
 	return (
 		<GithubContext.Provider
 			value={{
 				githubUser,
 				repos,
 				followers,
+				requests,
 			}}>
 			{children}
 		</GithubContext.Provider>
